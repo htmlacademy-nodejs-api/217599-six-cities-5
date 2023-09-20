e4eff8363ed0b1f3b3afedaad12901ce8e3c4cd1
pavel <shirokuiuna@mail.ru>
2023-09-20 20:23:23 +0300

    Менеджер CLI

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -7,6 +7,8 @@ type CommandCollection = Record<string, Command>;
 export class CLIApplication {
   private commands: CommandCollection = {};
 
+  constructor(private readonly defaultCommand: string = CommandName.HELP) {}
+
   public registerCommands(commandList: Command[]): void {
     commandList.forEach((command) => {
       if (Object.hasOwn(this.commands, command.getName())) {
@@ -14,5 +16,24 @@ export class CLIApplication {
       }
       this.commands[command.getName()] = command;
     });
+  }
+
+  public getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  public getDefaultCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+    }
+    return this.commands[this.defaultCommand];
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
   }
 }
