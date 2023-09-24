522730cc3033c5e98f6b0ca0bd31ccdae511583e
pavel <shirokuiuna@mail.ru>
2023-09-24 12:58:20 +0300

    Потоковое чтение файла

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,20 +1,34 @@
+import chalk from 'chalk';
+
 import { Command } from './command.interface.js';
 import { CommandName } from './command.types.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
+import { createAdvert } from '../../shared/helpers/index.js';
 
 export class ImportCommand implements Command {
-  getName(): string {
+  public getName(): string {
     return CommandName.IMPORT;
   }
 
-  execute(...params: string[]) {
+  private onImportedLine(line: string): void {
+    const offer = createAdvert(line);
+
+    console.log(offer);
+  }
+
+  private onCompleteImport(count: number): void {
+    console.info(chalk.blue(`${count} rows imported.`));
+  }
+
+  public execute(...params: string[]): void {
     const [filename] = params;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
+
     try {
       fileReader.read();
-
-      console.log(fileReader.toArray());
     } catch (err) {
       if (!(err instanceof Error)) {
         throw err;
