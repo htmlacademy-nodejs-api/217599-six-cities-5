9298681bef9cc45c51f7d7a6be42f6287d633bbc
pavel <shirokuiuna@mail.ru>
2023-09-30 12:43:19 +0300

    Считывание и вывод в консоль прочтенного .env файла

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,9 +1,11 @@
 import { PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from './shared/libs/config/rest.config.js';
 
 const bootstrap = () => {
   const logger = new PinoLogger();
-  const restApp = new RestApplication(logger);
+  const config = new RestConfig(logger);
+  const restApp = new RestApplication(logger, config);
 
   restApp.init();
 };
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,9 +1,14 @@
 import { Logger } from '../shared/libs/logger/index.js';
+import { Config } from '../shared/libs/config/config.interface.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(
+    private readonly logger: Logger,
+    private readonly config: Config,
+  ) {}
 
   init(): void {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
