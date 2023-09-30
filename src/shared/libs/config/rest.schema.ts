03cbd09db0acf1fed760d309ae345b468aaffc70
pavel <shirokuiuna@mail.ru>
2023-09-30 13:10:38 +0300

    Расширил схему валидирования .env файла

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -1,14 +1,34 @@
 import convict from 'convict';
+import validators from 'convict-format-with-validator';
+
+const DEFAULT_DB_HOST = '127.0.0.1';
+const DEFAULT_PORT = 4000;
 
 export type RestSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 };
+
+convict.addFormats(validators);
 
 export const configRestSchema = convict<RestSchema>({
   PORT: {
     doc: 'Port for incoming connections',
     format: 'port',
     env: 'PORT',
-    default: 4000,
+    default: DEFAULT_PORT,
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: 'string',
+    env: 'SALT',
+    default: null,
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: DEFAULT_DB_HOST,
   },
 });
